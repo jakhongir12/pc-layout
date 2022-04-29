8774a688abd6fa11f7598f970f85f64be90b15a9
Jakhongir <77021074+jakhongir12@users.noreply.github.com>
2022-04-30 01:17:42 +0500

    Add files via upload

--- PC-layout/js/accordion.js
+++ PC-layout/js/accordion.js
@@ -1,10 +1,24 @@
-const contents = document.querySelectorAll(".program-line__content");
+const accordion = () => {
+  const contents = document.querySelectorAll(".program-line__content");
+  const contentsDescr = document.querySelectorAll(".program-line__descr");
 
-contents.forEach((elem) => {
-  const title = elem.querySelector(".program-line__title");
-  const descr = elem.querySelector(".program-line__descr");
+  contents.forEach((elem) => {
+    const title = elem.querySelector(".program-line__title");
+    const descr = elem.querySelector(".program-line__descr");
 
-  title.addEventListener("click", () => {
-    descr.classList.toggle("active");
+    descr.style.transition = "height .3s";
+    descr.style.overflow = "hidden";
+
+    title.addEventListener("click", () => {
+      contentsDescr.forEach((tab) => {
+        if (tab === descr) {
+          tab.style.height = tab.scrollHeight + "px";
+        } else {
+          tab.style.height = 0;
+        }
+      });
+    });
   });
-});
+};
+
+accordion();
--- PC-layout/js/smoothScroll.js
+++ PC-layout/js/smoothScroll.js
@@ -1,18 +1,22 @@
-const navbar = document.querySelector(".header__nav");
-const links = navbar.querySelectorAll("a");
+const smoothScroll = () => {
+  const navbar = document.querySelector(".header__nav");
+  const links = navbar.querySelectorAll("a");
 
-links.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log(seamless);
-    const section = document.querySelector(link.getAttribute("href"));
+  links.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log(seamless);
+      const section = document.querySelector(link.getAttribute("href"));
 
-    if (section) {
-      seamless.scrollIntoView(section, {
-        behavior: "smooth",
-        block: "start",
-        inline: "center",
-      });
-    }
+      if (section) {
+        seamless.scrollIntoView(section, {
+          behavior: "smooth",
+          block: "start",
+          inline: "center",
+        });
+      }
+    });
   });
-});
+};
+
+smoothScroll();
